949215afb6d6375a936059b1eac815126c9f21c8
Ethan <q40131125@gmail.com>
2024-03-27 16:40:05 +0800

    Initial Commit

---
+++ TicTacToe_swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe_swiftUI/TicTacToe_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_swiftUIApp.swift
+//  TicTacToe_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/27.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
